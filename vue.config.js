953d1a332ad0a7257c59e8ae145b4234e29278a0
Aelita <xsjctony@126.com>
2021-11-29 16:45:07 +1100

    Bugfix: duplicate Loading element

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const { JSDOM } = require('jsdom')
+
 module.exports = {
   configureWebpack: {
     module: {
@@ -35,6 +37,13 @@ module.exports = {
         const regExp = /<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">/gi
         const res = route.html.match(regExp)
         route.html = route.html.replace(res[0], '')
+
+        const html = new JSDOM(route.html)
+        const document = html.window.document
+        const loadingElement = document.querySelector('.container')
+        loadingElement.remove()
+
+        route.html = html.serialize()
         return route
       }
     }
